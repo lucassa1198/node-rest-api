c4a1b46f8994b389c39981afd4d1be8c65674f8e
Lucas André <tavareslucas383@gmail.com>
2026-02-05 20:07:16 -0300

    feat: add logical filter to send users

---
+++ src/mocks/user.js
@@ -0,0 +1,10 @@
+module.exports = [{
+    id: 1,
+    nome: 'Lucas André'
+    }, {
+    id: 2,
+    nome:'Linekker'
+    },{
+        id:3,
+        nome: 'Luã'
+    }];
--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 const http =require('node:http');
+const user = require('./mocks/user');
+const userJson =JSON.stringify(user);
 
 http.createServer(function(request, response){
+if (request.url ==='/users'){
+    response.writeHead(200,
+    {'content-type': 'application/json'}
+);
+response.end(userJson);
+return;
+}
+
     response.writeHead(200,{'content-type': 'text/html; charset=utf-8'});
 response.end('<h1>Olá cliente</h1>');
 }).listen(3000);
